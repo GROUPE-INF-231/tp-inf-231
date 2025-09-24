f6da9355a9397380290e54a14ee122042f028df4
Fagna Noel <fnoelvanys@gmail.com>
2025-09-24 17:14:47 +0200

    Update trie tableau

--- trie_tab.c
+++ trie_tab.c
@@ -1,42 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 100
 
-int is_trie_tab(double tab[],int TailleMax){
+int ordre_tri(double tab[], int taille) {
+    int i;
+    int croissant = 1;
+    int decroissant = 1;
 
-    for(size_t i=0;i<TailleMax -1;i++){
-        if(tab[i]>tab[i+1])
-            return 0;
-        }
-
-    
-    return 1;
-}
-
-
-int main(){
-    int Taille;
-
-    printf("entrer la taille de votre tableau:");
-
-    scanf("%d",&Taille);
-
-    double tab[Taille];
-
-    printf("remplissafe du tableau...");
-    printf("\n");
-    for(size_t i=0;i<Taille;i++){
-        scanf("%lf",&tab[i]);
+    for (i = 0; i < taille - 1; i++) {
+        if (tab[i] > tab[i + 1])
+            croissant = 0;
+        if (tab[i] < tab[i + 1])
+            decroissant = 0;
     }
 
-    if(is_trie_tab(tab,Taille))
-        printf("le tableau est pas trié");
+    if (croissant)
+        return 1;
+    else if (decroissant)
+        return 2;
     else
-        printf("le tableau n'est pas trié");
-    
+        return 0;
+}
+
+int main() {
+    double tableau[MAX];
+    int taille = 0;
+    double x;
+
+    printf("Entrez les elements du tableau: (-999 pour terminer):\n");
+    do {
+        scanf("%lf", &x);
+        if (x != -999) {
+            if (taille < MAX) {
+                tableau[taille++] = x;
+            }
+            else {
+                printf("Taille maximale (%d) atteinte.\n", MAX);
+                break;
+            }
+        }
+    } while (x != -999);
 
 
-        printf("\n");
+    int ordre = ordre_tri(tableau, taille);
+    if (ordre == 1)
+        printf("Le tableau est trie dans l'ordre croissant.\n");
+    else if (ordre == 2)
+        printf("Le tableau est trie dans l'ordre decroissant.\n");
+    else
+        printf("Le tableau n'est pas trie.\n");
 
-return 0;
-
+    return 0;
 }
